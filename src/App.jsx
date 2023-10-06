64b22f295afcfe2ed57f6251f45e74c60c572c0d
Thomas Maghanga <142288589+tom-maghanga@users.noreply.github.com>
2023-10-06 06:36:58 +0300

    new react project practice

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me !"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Cat",
+      breed: "Cockatiel",
+    }),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
